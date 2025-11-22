d0daecdf29a00acfd5ea57d833a0107ac5cd56b3
Thiago da Silva <ts930201@gmail.com>
2025-11-21 22:11:20 -0300

    first commit

---
+++ src/main/java/com/thiago/db/DbIntegretyException.java
@@ -0,0 +1,10 @@
+package com.thiago.db;
+
+public class DbIntegretyException extends  RuntimeException{
+    private  static  final  long serialVersionUID = 1L;
+
+    public DbIntegretyException(String msg){
+        super(msg);
+    }
+
+}
