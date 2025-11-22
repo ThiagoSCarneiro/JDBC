a0c1f2c183317b500cb8a2316a2a512d84efa183
Thiago da Silva <ts930201@gmail.com>
2025-11-21 22:28:51 -0300

    Seller entity class

--- src/main/java/com/thiago/App.java
+++ src/main/java/com/thiago/App.java
@@ -1,11 +1,16 @@
 package com.thiago;
 
 import com.thiago.model.entities.Department;
+import com.thiago.model.entities.Seller;
+
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
         Department dp = new Department(1L, "Thiago");
+        Seller sl = new Seller(1L, "Thiago", "thiago@gmail.com", new Date(), 3000.0, dp);
         System.out.println(dp.toString());
+        System.out.println(sl.toString());
 
     }
 }
