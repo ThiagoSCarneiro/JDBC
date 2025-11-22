66a1cc2b071a723dec46ef90785762dfdec04bd5
Thiago da Silva <ts930201@gmail.com>
2025-11-21 22:22:01 -0300

    Department entity class

--- src/main/java/com/thiago/App.java
+++ src/main/java/com/thiago/App.java
@@ -1,9 +1,11 @@
 package com.thiago;
 
+import com.thiago.model.entities.Department;
+
 public class App {
     public static void main(String[] args) {
-
-
+        Department dp = new Department(1L, "Thiago");
+        System.out.println(dp.toString());
 
     }
 }
